a229d5fe5fcb5f96fc8fd6afc0aecb6f8b2f657f
ThecodingStudent <a360069@uach.mx>
2026-06-24 22:50:45 -0600

    UI Optimization

--- server/static/js/navbar.js
+++ server/static/js/navbar.js
@@ -4,17 +4,11 @@ if (navbarRoot) {
   navbarRoot.innerHTML = `
     <nav class="app-navbar" aria-label="Navegacion principal">
       <a class="brand" href="/">
-        <span class="brand-mark" aria-hidden="true">Shaparro</span>
         <span>N. I. N. A</span>
       </a>
       <div class="nav-links">
         <a href="/">CORE</a>
         <a href="/tools">EDITOR</a>
-      </div>
-      <div class="nav-meta" aria-hidden="true">
-        <span>3.19 GHz</span>
-        <span>NET</span>
-        <span>LOCAL</span>
       </div>
     </nav>
   `;
